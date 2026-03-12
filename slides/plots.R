a99e7c8cccf454901aa8869d2affe7ac85a6bb35
nikiforos <nikgzak@yahoo.com>
2026-03-12 20:30:00 +0100

    Added Roboto fonts for use in other applications

--- slides/plots.R
+++ slides/plots.R
@@ -1,8 +1,13 @@
 library(tidyverse)
+library(sysfonts)
 library(showtext)
 
-# Load Roboto font from Google Fonts
-sysfonts::font_add_google(name = "Roboto", family = "roboto")
+font_add("Roboto-Regular", "slides/fonts/Roboto-Regular.ttf")
+font_add("Roboto-Bold", "slides/fonts/Roboto-Bold.ttf")
+font_add("Roboto-Italic", "slides/fonts/Roboto-Italic.ttf")
+font_add("Roboto-BoldItalic", "slides/fonts/Roboto-BoldItalic.ttf")
+
+showtext_auto()
 
 scale_data = tibble(score = seq(2, 10, length.out = 200))
 
@@ -16,9 +21,9 @@ scale_score_p = ggplot(scale_data, aes(x = 1, y = score, fill = score)) +
     breaks = c(2, 6, 10),
     labels = c("2", "6", "10")
   ) +
-  annotate("text", x = 1.08, y = 10, label = "High transparency", hjust = 0, size = 10/3) +
-  annotate("text", x = 1.08, y = 6, label = "Medium transparency", hjust = 0, size = 10/3) +
-  annotate("text", x = 1.08, y = 2, label = "Low transparency", hjust = 0, size = 10/3) +
+  annotate("text", x = 1.08, y = 10, label = "High", hjust = 0, size = 10/3, family = "Roboto-Regular") +
+  annotate("text", x = 1.08, y = 6, label = "Medium", hjust = 0, size = 10/3, family = "Roboto-Regular") +
+  annotate("text", x = 1.08, y = 2, label = "Low", hjust = 0, size = 10/3, family = "Roboto-Regular") +
   labs(
     x = NULL,
     y = "Total transparency score",
@@ -31,12 +36,14 @@ scale_score_p = ggplot(scale_data, aes(x = 1, y = score, fill = score)) +
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     panel.grid = element_blank(),
-    plot.title = element_text(hjust = 0, face = "bold"),
+    plot.title = element_text(hjust = 0, family = "Roboto-Bold"),
     axis.title.y = element_blank(),
-    text = element_text(family = "Roboto")# horizontal on top
+    text = element_text(family = "Roboto-Regular")# horizontal on top
   )
 
 # Save plot
-ggsave("slides/images/scale_score_p.svg", scale_score_p,width = 7.5,   # 7.5 cm for narrow column
-       height = 7, 
-       units = "cm")
+ggsave("slides/images/scale_score_p.svg", scale_score_p, width = 7.5,   # 7.5 cm for narrow column
+       height = 5, 
+       units = "cm", dpi = 300,
+       device = svglite::svglite,
+       )
